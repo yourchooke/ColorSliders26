982e4d0e0e06d904f130d7e7e429be34db0cf655
Olga Yurchuk <olga.yurchuk@xored.com>
2022-07-13 22:45:54 +0700

    Colored view is added

---
+++ ColorSliders22/ColoredViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ColoredViewController.swift
+//  ColorSliders26
+//
+//  Created by Olga Yurchuk on 13.07.2022.
+//
+
+import UIKit
+
+class ColoredViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
